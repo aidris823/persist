2271b4bb834c1256f279c24690ad39b25d506506
aidris823 <pokeanand@gmail.com>
2019-01-18 01:26:57 -0500

    Okay cool.  SOrry again about latness =(

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,28 @@
 #include "pipe_networking.h"
 
 
+static void sighandler(int signo){
+  if (signo == SIGINT){
+    remove("Luigi");
+    exit(1);
+  }
+}
+
 int main() {
 
   int to_client;
   int from_client;
 
-  from_client = server_handshake( &to_client );
+  while (1){
+    //fork();
+    from_client = server_handshake( &to_client );
+    char * steve = calloc(100,1);
+    while(read(from_client,steve,100)){
+	printf("Client says: %s",steve);
+	char * appendix = "[This was understood by the server.]";
+	strcat(steve,appendix);
+	write(to_client,steve,100);
+    }
+  }
 }
+   
--- client.c
+++ client.c
@@ -7,4 +7,16 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+  char * read_steve = calloc(100,1);
+  char * write_steve = calloc(100,1);
+
+  while(1){
+    //fork();
+    printf("Welcome to the warp zone! Enter which world: (Or anything really \n");
+    fgets(read_steve,100,stdin);
+    read_steve[strlen(read_steve)-1] = (char)'/0';//Apparently this is how to make a null charNULL;
+    write(to_server,read_steve,100);
+    read(from_server,write_steve,100);
+    printf("Survey[er] says... %s\n",write_steve);
+  }
 }
